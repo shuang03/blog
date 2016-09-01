4c4477283724ec426febe33185ba5fd36e38a334
Shanshan Huang <Shanshan Huang>
2016-09-01 12:12:59 -0500

    adding blog to git repo

---
+++ resources/views/app.blade.php
@@ -0,0 +1,18 @@
+
+ * Created by PhpStorm.
+ * User: shans
+ * Date: 8/30/2016
+ * Time: 1:43 PM
+ */<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Blog</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+@yield('content')
+</div>
+</body>
+</html>
